2cf7267d4080e267fcf006ae8cb13fe22d4624ff
Rebecca B <rebeccabrown1234@gmail.com>
2018-05-21 10:46:35 -0400

    updates to code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,70 @@
 import React, { Component } from 'react';
+import Navbar from "./Components/Navbar";
+import Headerbar from "./Components/Headerbar";
+import Game from "./Components/Game";
+import Wrapper from "./Components/Wrapper";
+import Flower from "./Flowers.json";
 import './App.css';
-import Navbar from "./components/Navbar";
-import Headerbar from "./components/Headerbar";
-import Game from "./components/Game";
-import Wrapper from "./components/Wrapper";
-import Memory from "./Memory";
+
 
 
 class App extends Component {
 
-  state = { 
-            Memory: Memory,
-            gameNote: "Click an image to begin!",
-            highScore: 0,
-            score: 0
-          }
+  state = {
+    Flower: Flower,
+    Notice: "Click an image to begin.",
+    highScore: 0,
+    score: 0
+  };
 
   setClick = id => {
-    let currentIndex = this.state.Memory.findIndex(function(Memory){
-      return (id === Memory.id)
-    })
+    let currentIndex = this.state.Flower.findIndex(function(Flower){
+        return (id === Flower.id)
+    });
 
-    if (this.state.Memory[currentIndex].guessed === "no") {
-      let newImageArray = Memory.slice(0);
-      newImageArray[currentIndex].guessed = "yes";
+    if(this.state.Flower[currentIndex].guessed === "no"){
+      let newImages = Flower.slice(0);
+      newImages[currentIndex].guessed = "yes";
 
-      if (this.state.score > this.state.highScore) {
+      if(this.state.score > this.state.highScore){
         this.setState({
           score: this.state.score +1,
           highScore: this.state.score +1,
-          Memory: newImageArray,
-          gameNote: "Correct! Click another image."
+          Flower: newImages,
+          Notice: "Correct! Click another image."
         });
       }
-        else { 
-          this.setState({
-            score: this.state.score +1,
-            highScore: this.state.highScore,
-            Memory: newImageArray,
-            gameNote: "Correct! Click another image."
-          });
-    }
-
-      this.setState({
-        Memory:Memory.sort(function(a, b){return 0.5 - Math.random()})
-      });
-  } else {
-    if (this.state.score > this.state.highScore) {
-      this.setState({
-        Memory: Memory,
-        highScore: this.state.score,
-        score: 0,
-        gameNote: "Nope! Click another image."
-      });
-    }
       else{
         this.setState({
-          Memory: Memory,
+          score: this.state.score +1,
           highScore: this.state.highScore,
-          score: 0,
-          gameNote: "Nope! Click another image."
+          Flower: newImages,
         });
       }
-      for (let i = 0; i < Memory.length; i++) {
-        Memory[i].guessed = "no";
+
+
+      this.setState({
+        Flower: Flower.sort(function(a, b){return 0.5 - Math.random()})
+      });
+    } else {
+      if(this.state.score > this.state.highScore){
+        this.setState({
+          Flower: Flower,
+          highScore: this.state.score,
+          score: 0,
+          Notice: "Oh no! Click another image to start restart."
+        });
+      }
+      else{
+        this.setState({
+          Flower: Flower,
+          bestScore: this.state.bestScore,
+          score: 0,
+          gameNotice: "Oh no! Click another image to restart."
+        });
+      }
+      for(let i = 0; i < Flower.length; i++){
+        Flower[i].guessed = "no";
       }
     }
   };
@@ -72,26 +73,26 @@ class App extends Component {
     return (
       <div className="container-fluid">
         <Navbar
-          gameNote = {this.state.gameNote}
-          score = {this.state.score}
-          name = {this.state.name}
-          highScore = {this.state.highScore}
+            Notice= {this.state.gameNotice}
+            score= {this.state.score}
+            highScore={this.state.highScore}
         />
-          <Headerbar />
-          <Wrapper>
-            {this.state.Memory.map(Memory => (
-              <Game 
-                setClick = {this.setClick}
-                id = {Memory.id}
-                key = {Memory.id}
-                image = {Memory.image}
-                />
-            ))}
 
-            </Wrapper>
+        <Headerbar />
+        <Wrapper>
+          {this.state.Flower.map(Flower => (
+            <Game
+              setClick={this.setClick}
+              id={Flower.id}
+              key={Flower.id}
+              name={Flower.name}
+              image={Flower.image}
+            />
+        ))}
+        </Wrapper>
       </div>
-      );
-    }
+    );
   }
+}
 
 export default App;
